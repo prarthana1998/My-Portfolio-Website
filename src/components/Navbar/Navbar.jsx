8c2d128ae96621e9cc37f8d3c31d440dcbecf7dc
prarthana1998 <12.prarthanakandwal@gmail.com>
2024-03-19 23:03:14 +0000

    Adding updated code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React,{ userState }  from "react"
 import styles from "./Navbar.module.css"
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
   return (
 <nav className={styles.navbar}>
     <a className={styles.title} href="/">Portfolio</a>
     <div className={styles.menu}>
+      <img src={getImageUrl("nav/img.png")} alt="Menu"/>
         <ul className={styles.menuItems}>
-            <li><a href="#about">About </a></li>
+        <li><a  href="#about">About</a></li>
             <li><a href="#education">Education </a></li>
             <li><a href="#experience">Experience </a></li>
-            <li><a href="#skills">Skills </a></li>
+            <li><a href="#skills">Skills</a></li>
         </ul>
         
     </div>
